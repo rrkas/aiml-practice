790a842e6202092a3dffa01ea5c7981d66be4975
Rohnak Agarwal <rrka79wal@gmail.com>
2024-10-11 02:06:41 +0530

    aci-a1 setup

---
+++ projects/aci/readme.txt
@@ -0,0 +1,12 @@
+Artificial Computational Intelligence
+=====================================
+
+Assignment 1
+------------
+- A*
+- Genetic
+- Greedy Best first search
+- Hill climbing
+- IDA*
+- Random Restart Hill Climbing
+- Recursive Best First Search A*
---
+++ src/aci/state.py
@@ -0,0 +1,13 @@
+"""
+@book{aci-1,
+    title={Book: Artificial Intelligence - A Modern Approach},
+    author={{Stuart J. Russell and Peter Norvig}},
+    year={2010},
+    series={PRENTICE HALL SERIES IN ARTIFICIAL INTELLIGENCE},
+    publisher={Pearson},
+}
+"""
+
+
+class AbstractState:
+    pass
